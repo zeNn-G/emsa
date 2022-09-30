2ad80c322996077dbdce1b9c0406417c648cf424
zeNn-G <treroin@gmail.com>
2022-10-01 00:20:28 +0300

    First Commit

---
+++ emsaback/schemas/stores.js
@@ -0,0 +1,40 @@
+export default {
+  name: "stores",
+  title: "Stores",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "İsim",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Açıklama",
+      type: "string",
+    },
+    {
+      name: "discount",
+      title: "İndirim - %",
+      type: "number",
+    },
+    {
+      name: "address",
+      title: "Adres Linki (Google Maps)",
+      type: "string",
+    },
+    {
+      name: "district",
+      title: "Semt",
+      type: "string",
+    },
+    {
+      name: "imageurl",
+      title: "Resim",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
