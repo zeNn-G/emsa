91f396dd6d8e774fc17ecf97d230b83e3a4fb984
zeNn-G <treroin@gmail.com>
2022-10-01 17:55:49 +0300

    Fetch

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,27 @@ import { client } from "./sanityConfig";
 
 const App = () => {
   const [stores, setStores] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const query = '*[_type == "stores"]';
-
     client.fetch(query).then((data) => setStores(data));
   }, []);
+
+  useEffect(() => {
+    //Filter the stores based on the search input
+
+    const results = stores.filter((store) =>
+      store.name.toLowerCase().includes(search)
+    );
+    setStores(results);
+  }, [search]);
 
   return (
     <div className="bg-primary flex flex-col justify-center items-center">
       <div className="flex w-[80%] mt-[100px] ">
         <img src={Logo} alt="Emsa Gazi Logo" className="object-cover" />
+        <input type="text" onChange={(e) => setSearch(e.target.value)} />
       </div>
       <div className=" w-[80%] mt-[100px] bg-red-400">
         <div className="flex justify-start items-start">
