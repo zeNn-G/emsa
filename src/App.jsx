bbec97e4277f2068dd2cc8fb0507f1437c46f9ac
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-01 16:03:33 +0300

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,8 @@
 import React, { useEffect, useState } from "react";
-
-import { urlFor, client } from "./sanityConfig";
+import Card from "./components/Card";
 
 const App = () => {
-  const [stores, setStores] = useState([]);
-
-  useEffect(() => {
-    const query = '*[_type == "stores"]';
-
-    client.fetch(query).then((data) => setStores(data));
-  }, []);
-
-  return (
-    <div>
-      {stores.map((store) => {
-        return (
-          <>
-            <div key={store._id}>{store.description}</div>
-            <img src={urlFor(store.imageurl).url()} />
-          </>
-        );
-      })}
-    </div>
-  );
+  return <Card />;
 };
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+import { urlFor, client } from "../sanityConfig";
+
+const Card = () => {
+  const [stores, setStores] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "stores"]';
+
+    client.fetch(query).then((data) => setStores(data));
+  }, []);
+
+  return (
+    <div>
+      {stores.map((store) => {
+        return (
+          <>
+            <div key={store._id}>{store.description}</div>
+            <img src={urlFor(store.imageurl).url()} />
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Card;
