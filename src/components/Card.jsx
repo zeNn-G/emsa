ea6a17b697e5a7bd658dff38e9091f4283029558
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-01 17:55:32 +0300

    Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,15 +7,29 @@ const Card = ({ stores }) => {
     <div>
       {stores.map((store) => {
         return (
-          <div className="" key={store._id}>
-            <div className="">
-              <div className="">
-                <img src={urlFor(store.imageurl).url()} />
+          <div className="m-8" key={store._id}>
+            <div className="bg-green-200 w-[98vh] flex shadow-sm">
+              <div className="overflow-hidden h-52">
+                <img
+                  className="w-[200px] h-full object-cover block"
+                  src={urlFor(store.imageurl).url()}
+                />
               </div>
-              <div className="">
-                <div className="">
-                  <div className="">{store.name}</div>
-                  <div className="">{store.description}</div>
+              <div className="p-2">
+                <h2 className="name">{store.name}</h2>
+                <p className="desctription">{store.description}</p>
+
+                <div className="flex justify-between  flex-row">
+                  <div className="items-center">
+                    <h3 className="p-4 bg-blue-200 justify-start inline-block  rounded-xl">
+                      {store.district}
+                    </h3>
+                    <button className="ml-4">
+                      {/* {store.address} */}
+                      adress
+                    </button>
+                  </div>
+                  <div className="mt-4 end ">{store.discount}</div>
                 </div>
               </div>
             </div>
