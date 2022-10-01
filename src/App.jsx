b447d3a2adcffa07187903fb4e62550a303df34c
zeNn-G <treroin@gmail.com>
2022-10-01 14:57:31 +0300

    Arrow Func

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
-  return <div className="">Home</div>;
-}
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
 
 export default App;
