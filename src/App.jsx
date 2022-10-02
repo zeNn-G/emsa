b2b7c217e03d2ce442d7722d4d26703731773f93
zeNn-G <treroin@gmail.com>
2022-10-02 13:02:39 +0300

    Search

--- src/App.jsx
+++ src/App.jsx
@@ -19,38 +19,28 @@ const App = () => {
     });
   }, []);
 
-  const results = stores.filter((store) =>
-    store.name.toLowerCase().includes(search.toLowerCase())
-  );
-
-  useEffect(() => {
-    if (search.length > 0) {
-      // const filteredStores = stores.filter((store) => {
-      //   store.name.toLowerCase().includes(search.toLowerCase());
-      // });
-
-      if (results.length > 0) {
-        setStores(results);
-        setIsFound(false);
-      } else {
-        setIsFound(true);
-      }
-
-      console.log(results);
-      console.log(isFound);
-    } else {
-      setStores(originalStores);
-    }
-  }, [search]);
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    const filteredStores = originalStores.filter((store) => {
+      return store.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setStores(filteredStores);
+    setIsFound(filteredStores.length > 0);
+  };
 
   return (
     <div className="bg-primary flex flex-col justify-center items-center">
       <div className="flex w-[80%] mt-[100px] ">
         <img src={Logo} alt="Emsa Gazi Logo" className="object-cover" />
-        <input type="text" onChange={(e) => setSearch(e.target.value)} />
+        <input
+          type="text"
+          onInput={(e) => {
+            handleSearch(e);
+          }}
+        />
       </div>
       <div className=" w-[80%] mt-[100px] bg-red-400">
-        {!isFound ? <Card stores={stores} /> : null}
+        {isFound ? <Card stores={stores} /> : null}
       </div>
     </div>
   );
