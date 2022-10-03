5e5d24ba19503c536794e1158020d1a778372bd7
zeNn-G <treroin@gmail.com>
2022-10-04 00:05:24 +0300

    Bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Logo from "./assets/EmsaGazi.webp";
 
 import { client } from "./sanityConfig";
 import { data } from "./data";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 const App = () => {
   const [stores, setStores] = useState([]);
@@ -36,17 +37,25 @@ const App = () => {
 
   return (
     <div className="bg-primary flex flex-col justify-center items-center">
-      <div className="flex w-[80%] mt-[100px] ">
+      <div className="flex md:flex-row flex-col w-[80%] mt-[100px] justify-center">
         <img src={Logo} alt="Emsa Gazi Logo" className="object-cover" />
-        <input
-          type="text"
-          onInput={(e) => {
-            handleSearch(e);
-          }}
-        />
+        <div className="flex-row items-center space-x-2 pb-2 mx-4 md:w-[500px] mt-[20px] md:mt-0 ">
+          <div className="flex flex-row flex-1 space-x-2 bg-gray-200 p-3 rounded-xl mt-[8px]">
+            <MagnifyingGlassIcon className="w-[24px] h-[24px] " />
+            <input
+              type="search"
+              name="search"
+              placeholder="Search"
+              onInput={(e) => {
+                handleSearch(e);
+              }}
+              className="bg-transparent outline-none w-full text-black"
+            />
+          </div>
+        </div>
       </div>
 
-      <div className=" w-[80%] mt-[100px] bg-red-400">
+      <div className=" w-[80%] mt-[40px]">
         {isFound ? <Card stores={stores} /> : null}
       </div>
     </div>
