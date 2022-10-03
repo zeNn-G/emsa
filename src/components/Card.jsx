2d11acfbf87c80ae5faf4fa442d768fce9c3b2cb
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-04 00:05:08 +0300

    Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,26 +4,26 @@ import { urlFor } from "../sanityConfig";
 
 const Card = ({ stores }) => {
   return (
-    <div className="flex-wrap flex mt-10 items-center text-center justify-center">
+    <div className="flex-wrap relative md:flex  justify-center">
       {stores.map((store) => {
         return (
-          <div className="flex-wrap h-[300px] flex ml-10 justify-center items-center text-white gap-8">
-            <div className="bg-black/30 backdrop-blur-sm w-[400px] h-[250px] shadow-xl rounded-xl shadow-black flex flex-row hover:scale-110">
+          <div className="flex-row flex mt-10 justify-center items-center text-white gap-8">
+            <div className="backdrop-blur-sm  ml-0 mr-0 md:ml-5 md:mr-5  shadow-xl rounded-xl shadow-black sm:flex hover:scale-110">
               <div className="overflow-hidden text-center">
                 <img
-                  className="w-[500px] h-[250px] object-cover"
+                  className="md:w-[350px] w-[250px] h-[250px] md:h[350px] object-fit"
                   src={urlFor(store.imageurl).url()}
                 />
               </div>
-              <div className="flex w-full p-2 flex-col bg-slate-500">
-                <div className="text-2xl mb-4 flex-[3]">
+              <div className="flex sm:w-[250px] h-auto p-2 flex-col bg-slate-500">
+                <div className="text-2xl mb-4 flex-[1] md:flex-[3]">
                   <div className="border-b-2">{store.name}</div>
                   <div className="text-xl">{store.description}</div>
                 </div>
-                <div className="flex justify-around bg-purple-600 flex-[1]">
+                <div className="flex justify-around ">
                   <div className="flex ">
                     <div className=" p-2">{store.district}</div>
-                    <div className=" p-2">Icon</div>
+                    <button className=" p-2">Icon</button>
                   </div>
                   <div className="flex-[2] flex items-center justify-end">
                     <div className="">{store.discount} %</div>
