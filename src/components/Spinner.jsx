238af5dcc70c3260daf23ff370073aada89a9fec
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-05 22:38:32 +0300

    Spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -10,7 +10,7 @@ const Spinner = () => {
       >
         <svg
           role="status"
-          class="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600"
+          class="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -24,7 +24,7 @@ const Spinner = () => {
             fill="#1C64F2"
           />
         </svg>
-        Loading...
+        Yükleniyor...
       </button>
     </div>
   );
