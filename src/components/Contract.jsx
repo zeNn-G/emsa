05028092e18fbeffcd2ebcdc545e1ea18bc8a78c
zeNn-G <treroin@gmail.com>
2022-10-05 19:52:27 +0300

    Back Button

--- src/components/Contract.jsx
+++ src/components/Contract.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 
 const Contract = () => {
   const { state } = useLocation();
+  const navigate = useNavigate();
 
   return (
-    <div className="bg-primary flex justify-center items-center w-[100vw] h-[100vh]">
+    <div className="bg-primary flex flex-col justify-center items-center w-[100vw] h-[100vh]">
       <img src={state} className="object-cover" />
+      <button
+        className="bg-slate-700 p-3 mt-3 rounded-xl cursor-pointer flex items-center text-white font-medium tracking-wide"
+        onClick={() => navigate("/")}
+      >
+        <ChevronLeftIcon className="h-[40px] w-[40px] text-purpleAccent" />
+        Anasayfaya Dön
+      </button>
     </div>
   );
 };
