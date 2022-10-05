7ff0d3dd68b6f839f97feb9e9cb419fb72e67459
zeNn-G <treroin@gmail.com>
2022-10-05 19:59:10 +0300

    Hover Transition

--- src/components/Contract.jsx
+++ src/components/Contract.jsx
@@ -11,7 +11,7 @@ const Contract = () => {
     <div className="bg-primary flex flex-col justify-center items-center w-[100vw] h-[100vh]">
       <img src={state} className="object-cover" />
       <button
-        className="bg-slate-700 p-3 mt-3 rounded-xl cursor-pointer flex items-center text-white font-medium tracking-wide"
+        className="bg-slate-700 p-3 mt-3 rounded-xl cursor-pointer flex items-center text-white font-medium tracking-wide hover:bg-slate-600 transition-all duration-300 ease-in-out"
         onClick={() => navigate("/")}
       >
         <ChevronLeftIcon className="h-[40px] w-[40px] text-purpleAccent" />
