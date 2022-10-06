d2f0d5f7a0bb60c4ca1b2f75f9e253d7408d3289
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-06 18:50:46 +0300

    .

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,13 +14,13 @@ const Card = ({ stores }) => {
             key={store._id}
           >
             <div
-              className="flex flex-row backdrop-blur-sm ml-3 mr-3 md:ml-5 md:mr-5 md:shadow-lg rounded-xl shadow-purpleAccent
-            sm:max-w-[240px] md:max-w-[550px] max-w-[500px]
+              className="flex flex-row backdrop-blur-sm ml-3 mr-3 md:ml-5 md:mr-5 shadow-lg rounded-xl shadow-purpleAccent
+            max-w-[400px] md:max-w-[550px] 
             max-h-[350px] md:max-h-[250px] "
             >
               <div className="flex overflow-hidden text-center  md:rounded-none  rounded-sm bg-slate-700 md:rounded-l-xl md:w-[600px]">
                 <img
-                  className="md:w-[300px] w-[228px] h-[155px] md:h[250px] object-cover bg-slate-700"
+                  className="md:w-[300px] w-[350px] h-[200px] md:h[150px]  object-cover bg-slate-700"
                   src={urlFor(store.imageurl).url()}
                 />
               </div>
@@ -29,19 +29,21 @@ const Card = ({ stores }) => {
                   <h1 className="border-b-2 p-2 border-purpleAccent text-center font-semibold">
                     {store.name}
                   </h1>
-                  <h1 className="md:text-xl  text-center mt-2">
+                  <h1 className="md:text-xl text-sm text-center mt-2">
                     {store.description}
                   </h1>
                 </div>
                 <div className="flex border-t-2 border-purpleAccent">
-                  <div className="flex items-center gap-6 w-full">
-                    <div className="flex w-[70%] items-center">
-                      <h1 className="p-2">{store.district}</h1>
+                  <div className="flex flex-col sm:flex-row items-center gap-2 sm:gap-6 w-full">
+                    <div className="flex w-[70%]  items-center">
+                      <h1 className="mr-2 text-center flex">
+                        {store.district}
+                      </h1>
                       <a href={store.address} target="_blank">
                         <MapIcon className="h-6 w-6 text-purpleAccent" />
                         {/* <img src={Mapicon} className="h-[32px] w-[32px]" /> */}
                       </a>
-                      <div className="ml-1 cursor-pointer">
+                      <div className="ml-1 mr-5 cursor-pointer">
                         <Link
                           to={`/sozlesmeler/:${store.name}`}
                           state={store.contracturl}
@@ -52,7 +54,10 @@ const Card = ({ stores }) => {
                     </div>
                     <div className="">
                       <h1 className="">
-                        <span className="font-bold">{store.discount}</span> %
+                        <span className="font-bold text-sm">
+                          {store.discount}
+                        </span>{" "}
+                        %
                       </h1>
                     </div>
                   </div>
