97ede07c59f017fdd5fc6a8503052c3e5007f0f2
zeNn-G <treroin@gmail.com>
2022-10-15 14:03:58 +0300

    Scrollbar

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,7 +5,7 @@ import Spinner from "../components/Spinner";
 import Logo from "../assets/EmsaGazi.webp";
 
 import { client } from "../sanityConfig";
-import { data } from "../data";
+// import { data } from "../data";
 import { useQuery } from "react-query";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
